554c71a145db5907a9cfccefc4364f8ce215a703
qui3tK1D <zawhlyanhtet@gmail.com>
2022-10-10 22:28:29 +0630

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,24 @@ import SneakerInfo from "./components/SneakerInfo/SneakerInfo";
 const App = () => {
   return (
     <>
-      <div className="md:px-6 px-0 fixed z-10 bg-white w-full top-0">
+      <header className="md:px-6 px-0 fixed z-10 bg-white w-full top-0">
         <NavBar />
-      </div>
+      </header>
       <main>
         <section className="md:px-6 lg:px-16">
           <SneakerInfo />
         </section>
       </main>
+      <footer>
+        <div className="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            {" "}
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/qui3tK1D">Qui3tK1D</a>.
+        </div>
+      </footer>
     </>
   );
 };
--- src/components/UI/SliderBtn.jsx
+++ src/components/UI/SliderBtn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SliderBtn = ({ children, variants, ...rest }) => {
   return (
     <button
-      className={`w-10 h-10 bg-white text-xl md:text-2xl rounded-full flex items-center justify-center absolute top-1/2 -translate-y-1/2 shadow-xl ${variants}`}
+      className={`w-10 h-10 bg-white text-2xl rounded-full flex items-center justify-center absolute top-1/2 -translate-y-1/2 shadow-xl ${variants}`}
       {...rest}
     >
       {children}
